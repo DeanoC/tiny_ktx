83c785f3aa94640c9b778ea6641d802ba4387b3d
deano <me@deanoc.com>
2019-08-20 05:04:57 +0300

    Compile test fixes

--- src/tinyktx.c
+++ src/tinyktx.c
@@ -1,3 +1,3 @@
 #define TINYKTX_IMPLEMENTATION
-#include "tiny_imageformat/tinyimageformat.h"
+#include "tiny_imageformat/tinyimageformat_base.h"
 #include "tiny_ktx/tinyktx.h"
--- tests/test_tinyktx_tinyimageformat.cpp
+++ tests/test_tinyktx_tinyimageformat.cpp
@@ -1,3 +1,3 @@
 #include "al2o3_platform/platform.h"
-#include "tiny_imageformat/tinyimageformat.h"
+#include "tiny_imageformat/tinyimageformat_base.h"
 #include "tiny_ktx/tinyktx.h"
